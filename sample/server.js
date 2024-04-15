29fdc91aec8125c119b7389f551e783c2c2ca068
niba2828 <k.futaba2828@gmail.com>
2024-04-15 16:30:48 +0900

    Practice: styles.cssを適用できるように修正

--- sample/server.js
+++ sample/server.js
@@ -4,6 +4,25 @@ import { serve } from "https://deno.land/std@0.194.0/http/server.ts";
 
 // localhostにDenoのHTTPサーバを展開
 serve(async (request) => {
+    // パス名を取得する
+    // https://localhost:8000/hoge に接続した場合"/hoge"が取得できる
+    const pathname = new URL(request.url).pathname;
+    console.log(`pathname: ${pathname}`);
+    
+    // https://localhost:8000/styles.css へのアクセス時、"./public/styles.css"を返す
+    if (pathname === "/styles.css") {
+        const cssText = await Deno.readTextFile("./public/styles.css");
+        return new Response(
+            cssText,
+            {
+                headers: {
+                    // text/css形式のデータで、文字コードはUTF-8であること
+                    "Content-Type": "text/css; charset=utf-8"
+                }
+            }
+        );
+    }
+    
     const htmlText = await Deno.readTextFile("./public/index.html");
     return new Response(
         // Responseの第一引数にレスポンスのbodyを設置
