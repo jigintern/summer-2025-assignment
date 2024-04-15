1806d65cd54844db31501996fae2742b0ae49f11
niba2828 <k.futaba2828@gmail.com>
2024-04-15 11:47:17 +0900

    Practice: グローバル変数でアクセス回数をカウント

--- sample/server.js
+++ sample/server.js
@@ -2,7 +2,11 @@
 // denoではURLを直に記載してimportできます
 import { serve } from "https://deno.land/std@0.194.0/http/server.ts";
 
+// アクセス数を保持する変数をグローバル領域に定義
+let count = 0;
+
 // localhostにDenoのHTTPサーバを展開
 serve(request => {
-    return new Response("Hello Deno!");
+    count++;
+    return new Response(`Count: ${count}`);
 });
